fa505f7c5d545842810a7acf8fe00ea9a9a320c9
KomplexNZ <ethan.pandaram@kingsway.school.nz>
2024-09-25 10:22:23 +1200

    V1 - User Input
    This is the first version with user input.

---
+++ P_01_Basics.py
@@ -0,0 +1,21 @@
+#MY first Python program
+#Author: Ethan Pandadaram
+#Date: 2024-9-25
+#TODO:
+    #Get user input
+    # Ask the user for two number   
+    # add the number together
+
+
+#ask the user for their name
+name = input("What is your name?")
+print(f"Kia ora {name}.") #f is for formamt
+
+#ask the user for two numbers
+num_1 = int(input("What is your favourite number?"))
+num_2 = int(input("What is your least vavourite number?"))
+
+#add the numbers together
+
+sum = num_1 + num_2
+print(f"the numbers added together is equal to {sum}")
